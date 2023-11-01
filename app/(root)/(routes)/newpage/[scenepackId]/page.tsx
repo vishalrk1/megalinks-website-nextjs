364b427b91d090a1623bd56c0e370befbac9a235
Vishal.Karangale <Vishal.Karangale@ril.com>
2023-11-01 17:55:21 +0530

    bug fix

--- app/(root)/(routes)/newpage/[scenepackId]/page.tsx
+++ app/(root)/(routes)/newpage/[scenepackId]/page.tsx
@@ -1,27 +1,18 @@
-// "use client"
+"use client"
 
-// import { useAppDispatch, useAppSelector } from "@/redux/hooks";
-// import { getIndividualScenePack } from "@/redux/scenepacks/scenepackId/actions";
-// import { individualScenepackSelector } from "@/redux/scenepacks/scenepackId/selector";
-// import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { getIndividualScenePack } from "@/redux/scenepacks/scenepackId/actions";
+import { individualScenepackSelector } from "@/redux/scenepacks/scenepackId/selector";
+import { useEffect } from "react";
 
-// const ScenepackPage = ({
-//     params
-// }: {params : {scenepackId: string}}) => {
-//     const dispatch = useAppDispatch();
-//     const {data, pending, error} = useAppSelector(individualScenepackSelector);
+const ScenepackPage = ({
+    params
+}: {params : {scenepackId: string}}) => {
 
-//     useEffect(() => {
-//         dispatch(getIndividualScenePack(params.scenepackId));
-//     }, [dispatch]);
+    return (
+        <div>
+        </div>
+    );
+};
 
-//     return (
-//         <div>
-//             {pending && `Loading Scenepack`}
-//             {data && data.label}
-//             {error && `Something Went Wrong`}
-//         </div>
-//     );
-// };
-
-// export default ScenepackPage;
+export default ScenepackPage;
