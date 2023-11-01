bad107766edb2fdac1d1d946c27d6ff82a58688e
Vishal.Karangale <Vishal.Karangale@ril.com>
2023-11-01 17:57:02 +0530

    page updates

--- app/(root)/(routes)/newpage/page.tsx
+++ app/(root)/(routes)/newpage/page.tsx
@@ -5,16 +5,8 @@ import { scenepackSelector } from "@/redux/scenepacks/selector";
 import Link from "next/link";
 
 const NewPage = () => {
-  const { data, pending, error } = useAppSelector(scenepackSelector);
   return (
     <div className="p-10">
-      <div className="flex flex-col space-y-4">
-        {data.map((item) => (
-          <Link href={`newpage/${item.id}`} key={item.id}>
-            <h2>{item.label}</h2>
-          </Link>
-        ))}
-      </div>
     </div>
   );
 };
